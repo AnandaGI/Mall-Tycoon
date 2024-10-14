3284279b9e226aa5392f56fddc6e3d954ae53588
AnandaGI <agirwin42@tntech.edu>
2024-10-14 13:31:56 -0500

    Add option to add new store to mall.
    Update mall with finished functions.

--- driver.py
+++ driver.py
@@ -22,27 +22,56 @@ for i in range(0, 3):
     print(".", end = "")
 
 print("\n\n" + str(user_mall) + ", eh? That sounds great! Let's begin:")
+time.sleep(2)
 
 while run_program:
 
     print("\nActions:")
     print("1)\tCreate New Store\n2)\tEdit Existing Store\n3)\tLoad Data From File\n4)\tSave Data To File")
-    user_option = int(input("Choose option 1-4:\t"))
-    while 4 < user_option < 1:
-        user_option = input("Invalid choice. Must type a number 1-4:\t")
+    print("5)\tDisplay Stores\n6)\tEnter User Perspective\n7)\tExit Program")
+    user_option = int(input("Choose option 1-5:\t"))
+    while user_option < 1 or user_option > 7:
+        user_option = int(input("Invalid choice. Must type a number 1-6:\t"))
 
     match user_option:
+        #Case 1, The user will add a new store to the mall.
         case 1:
-            pass
+            #Gain input for the name, description, and type of store.
+            store_name = input("\nWhat is your new store's name?\t")
+            store_description = input("Give a short description of your store:\t")
+            store_type = int(input("Does your store sell goods (1), services (2), or both?(3)\t"))
+            while store_type < 1 or store_type > 3:
+                store_type = int(input("\nInvalid option. Does your store sell goods (1), services (2), or both?(3)\t"))
+
+            #Create the new store
+            match store_type:
+                case 1:
+                    new_store = RetailStore(store_name, store_description)
+                case 2:
+                    new_store = ServiceStore(store_name, store_description)
+                case 3:
+                    new_store = ComboStore(store_name, store_description)
+
+            user_mall.add_store(new_store)  #Add new store to the mall
+            print("\n" + str(new_store) + " has been successfully added to " + str(user_mall))
+            input("Press ENTER to continue.")
 
 
         case 2:
             pass
         case 3:
             pass
-        case 3:
+        case 4:
             pass
 
+        case 5:
+            user_mall.display_mall()
+            input("\nPress ENTER to continue.")
+
+        case 6:
+            pass
+        case 7:
+            run_program = False
 
 
-    run_program = False
+
--- mall.py
+++ mall.py
@@ -4,10 +4,20 @@ Purpose:    Create stores, which sell items, services, or both
 Updated:    10/14/2024
 """
 
+from store import Store
+
 class Mall:
     def __init__(self, name):
         self.name = name
-        self.mall_list = []
+        self.store_list = []
 
     def __str__(self):
         return self.name
+
+    def add_store(self, store: Store):
+        self.store_list.append(store)
+
+    def display_mall(self):
+        print("\n" + self.name + " Current Store List\n" + ("-"*20))
+        for store in self.store_list:
+            print(store, "|", store.get_description())
