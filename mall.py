ef0d6490949a8515b87dcbcbc70a4ae32787149c
AnandaGI <agirwin42@tntech.edu>
2024-10-14 14:50:35 -0500

    Add getter functions

--- mall.py
+++ mall.py
@@ -10,14 +10,27 @@ class Mall:
     def __init__(self, name):
         self.name = name
         self.store_list = []
+        self.num_stores = 0
 
     def __str__(self):
         return self.name
 
     def add_store(self, store: Store):
         self.store_list.append(store)
+        self.num_stores += 1
+
+    def get_num_stores(self):
+        return self.num_stores
+
+    def get_store(self, index):
+        return self.store_list[index]
 
     def display_mall(self):
         print("\n" + self.name + " Current Store List\n" + ("-"*20))
         for store in self.store_list:
             print(store, "|", store.get_description())
+
+    def display_store_list(self):
+        print("\n" + self.name + " Current Store List\n" + ("-" * 20))
+        for i in range(0, len(self.store_list)):
+            print((i+1), ")\t", self.store_list[i])
