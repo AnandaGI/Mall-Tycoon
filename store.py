a46479137ce7ecaa0576ba6aafb677a5d4e25aa8
AnandaGI <agirwin42@tntech.edu>
2024-10-21 17:48:10 -0500

    Update store.py

--- store.py
+++ store.py
@@ -40,13 +40,59 @@ class Plot:
 
 
 """
+Generic Store Class, subclass of a simple plot
+"""
+class Retailer(Plot):
+    def __init__(self, name: str, description: str, square_feet: int):
+        super().__init__(name, description, square_feet)
+        self.__product_list = []
+
+    @property
+    def list(self):
+        return self.__product_list
+
+    def add_product(self, product: Product):
+        self.__product_list.append(product)
+        return 1
+
+    def remove_product(self, product_name: str):
+        for product in self.__product_list:
+            if product_name == product.name:
+                self.__product_list.remove(product)
+                return 1
+        else:
+            print("\nProduct not found in catalog.")
+            return 0
+
+    #Prints all the objects of some class type
+    def list_type(self, start_index: int, product_type):
+        i = start_index
+        for curr_product in self.__product_list:
+            if isinstance(curr_product, product_type):
+                if product_type == Item:
+                    print(str(i) + ")\t" + curr_product.name + " | " + curr_product.stock)
+                elif product_type == Service:
+                    print(str(i) + ")\t" + curr_product.name + " | " + curr_product.time_range)
+                else:
+                    print(str(i) + ")\t" + curr_product.name)
+            i += 1
+
+    def display_catalog(self):
+        print("\n" + self.name + "'s Overview Page\n" + ("-" * 20))
+
+        # If no products exist to display, notify user
+        if len(self.__product_list) == 0:
+            print("No products to display.")
+            return
+        
+
+"""
 Restaurant - has a limited capacity and a menu of food items
 """
-class Restaurant(Plot):
+class Restaurant(Retailer):
     def __init__(self, name: str, description: str, sqr_feet: int, seats: int):
         super().__init__(name, description, sqr_feet)
         self.__seats = seats #How many guests can the restaurant seat?
-        self.__menu = []
 
     @property
     def seats(self):
@@ -56,32 +102,18 @@ class Restaurant(Plot):
     def seats(self, num_seats: int):
         self.__seats = num_seats
 
-    @property
-    def list(self):
-        return self.__menu
-
     #Could hypothetically create new product type recipe that utilizes other products in its composition, setting
     #the restaurant class apart from its peers somewhat
     def add_product(self, food: Item):
         if isinstance(food, Item):
-            self.__menu.append(food)
-            return 1
+            super().add_product(food)
         else:
             return 0
 
-    def display_menu(self):
-        #If no products exist to display, notify user
-        if len(self.__menu) == 0:
-            print("No products to display.")
-            return
-
-        i = 1
-        #Print all items
-        for curr_product in self.__menu:
-            if isinstance(curr_product, Item):
-                print(str(i) + ")\t" + curr_product.name + " | " + curr_product.stock)
-                i += 1
-
+    def display_catalog(self):
+        super().display_catalog()
+        print("Available Dishes:")
+        self.list_type(1, Item)
         print("-" * 20)
         #End
 
@@ -89,68 +121,39 @@ class Restaurant(Plot):
 """
 Department - an function of the mall for guests to interact with, such as security or customer service
 """
-class Department(Plot):
+class Department(Retailer):
     def __init__(self, name: str, description: str, sqr_feet: int):
         super().__init__(name, description, sqr_feet)
-        souvenir = Product(self.name + " Department Souvenir", "A souvenir from the " + self.name + " Department.")
-        self.__functions = [souvenir]
-
-    @property
-    def list(self):
-        return self.__functions
 
     def add_product(self, service: Service):
         if isinstance(service, Service):
-            self.__functions.append(service)
-            return 1
+            super().add_product(service)
         else:
             return 0
 
-    def display_department(self):
+    def display_catalog(self):
+        super().display_catalog()
         print("\nHi, welcome to " + self.name + "!\nWe can provide you with the following services:")
-        i = 1
-        for curr_product in self.__functions:
-            if isinstance(curr_product, Service):
-                print(str(i) + ")\t" + curr_product.name)
-                i += 1
+        self.list_type(1, Service)
+
 
 """
 Combo Store - sells both services and items
 """
-class Store(Plot):
+class Store(Retailer):
     def __init__(self, name: str, description: str, sqr_feet: int):
         super().__init__(name, description, sqr_feet)
-        self.__product_catalog = []
-
-    @property
-    def list(self):
-        return self.__product_catalog
-
-    def add_product(self, product: Product):
-        self.__product_catalog.append(product)
-        return 1
 
     def display_catalog(self):
-        print("\n" + self.name + "'s Product Page\n" + ("-" * 20))
-
-        #If no products exist to display, notify user
-        if len(self.__product_catalog) == 0:
-            print("No products to display.")
-            return
+        super().display_catalog()
 
         i = 1
         #Print all items
         print("Products:")
-        for curr_product in self.__product_catalog:
-            if isinstance(curr_product, Item):
-                print(str(i) + ")\t" + curr_product.name + " | " + curr_product.stock)
-                i += 1
+        self.list_type(i, Item)
 
         print("Services:")
-        for curr_product in self.__product_catalog:
-            if isinstance(curr_product, Service):
-                print(str(i) + ")\t" + curr_product.name)
-                i += 1
+        self.list_type(i, Service)
 
         print("-" * 20)
         #End
