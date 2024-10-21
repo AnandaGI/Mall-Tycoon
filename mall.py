594051f26b9908d457289172fcbd6a16f61b748d
AnandaGI <agirwin42@tntech.edu>
2024-10-21 01:59:39 -0500

    Update mall to have product lists

--- mall.py
+++ mall.py
@@ -5,32 +5,57 @@ Updated:    10/14/2024
 """
 
 from store import Plot
+from product import Product
 
 class Mall:
     def __init__(self, name):
-        self.name = name
-        self.store_list = []
-        self.num_stores = 0
+        self.__name = name
+        self.__store_list = []          #List of stores
+        self.__num_stores = 0           #Number of stores in mall
+        self.__all_products = []        #All products that have been created during the course of the mall's lifespan
+        self.__active_products = []     #The current products that are being created or used
 
-    def __str__(self):
+    @property
+    def name(self):
         return self.name
 
     def add_store(self, plot: Plot):
-        self.store_list.append(plot)
-        self.num_stores += 1
+        self.__store_list.append(plot)
+        self.__num_stores += 1
 
-    def get_num_stores(self):
-        return self.num_stores
+    @property
+    def num_stores(self):
+        return self.__num_stores
 
     def get_store(self, index):
-        return self.store_list[index]
+        return self.__store_list[index]
 
     def display_mall(self):
         print("\n" + self.name + " Current Store List\n" + ("-"*20))
-        for store in self.store_list:
+        for store in self.__store_list:
             print(store, "|", store.get_description())
 
     def display_store_list(self):
         print("\n" + self.name + " Current Store List\n" + ("-" * 20))
-        for i in range(0, len(self.store_list)):
-            print((i+1), ")\t", self.store_list[i])
+        for i in range(0, len(self.__store_list)):
+            print((i+1), ")\t", self.__store_list[i])
+
+    #This will go through all the stores and delete all instances of some particular product
+    def recall_product(self, product_name: str):
+        for store in self.__store_list:
+            for product in store.list:
+                if product_name == product.name:
+                    store.list.remove()
+
+    def add_product(self, product: Product):
+        self.__active_products.append(product)
+        self.__active_products.append(product)
+
+    def clear_products(self):
+        self.__active_products.clear()
+
+    def display_products(self):
+        print("\nAll Previous Products\n" + ("-" * 20))
+        i = 1
+        for product in self.__all_products:
+            print(i, ")\t", product.name)
