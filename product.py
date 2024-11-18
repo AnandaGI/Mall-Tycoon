cbc8c58a18baf44bd529351a5a91979015fd213c
AnandaGI <agirwin42@tntech.edu>
2024-11-17 22:58:18 -0600

    Update product classes

--- product.py
+++ product.py
@@ -8,9 +8,13 @@ Updated:    10/14/2024
 General Product Class - Simply placeholder than can be put on shelves
 """
 class Product:
-    def __init__(self, name: str, description = ""):
+    def __init__(self, name: str, description: str, price: float = 0):
+        if price < 0.0:
+            raise ValueError
+
         self.__name = name
         self.__description = description
+        self.__price = price
 
     def __dict__(self):
         return {
@@ -38,6 +42,16 @@ class Product:
     def description(self, description: str):
         self.__description = description
 
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price < 0.0:
+            raise ValueError
+        self.__price = new_price
+
     def print_stats(self):
         print(self.__name + " Stats\nDescription: " + self.__description)
 
@@ -46,15 +60,18 @@ Item Class - Product that has stock and can be sold or ordered.
 """
 class Item(Product):
     def __init__(self, name: str, description, price: float, stock: int = 0):
-        super().__init__(name, description)
+        super().__init__(name, description, price)
         self.__stock = stock
-        self.__price = price
 
     def __dict__(self):
         item_dict = super().__dict__()
-        item_dict["price"] = self.__price
+        item_dict["price"] = self.price
         item_dict["stock"] = self.__stock
         return item_dict
+
+    @property
+    def stock(self):
+        return self.__stock
 
     def restock(self, amount: int):
         self.__stock += amount
@@ -64,44 +81,23 @@ class Item(Product):
 
     def print_stats(self):
         super().print_stats()
-        print("Price: " + str(self.__price) + "\nStock: " + str(self.__stock))
-
-    @property
-    def stock(self):
-        return self.__stock
-
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, new_price: float):
-        self.__price = new_price
+        print("Price: " + str(self.price) + "\nStock: " + str(self.__stock))
 
 """
 Service Class - Service that has timeslots and can be booked
 """
 class Service(Product):
-    def __init__(self, name: str, description, price: float, start_time: str = "8:00 AM", end_time: str = "7:00 PM"):
-        super().__init__(name, description)
-        self.__price = price
+    def __init__(self, name: str, description: str, price: float, start_time:str = "8:00 AM", end_time:str = "7:00 PM"):
+        super().__init__(name, description, price)
         self.__start_time = start_time
         self.__end_time = end_time
 
     def __dict__(self):
         item_dict = super().__dict__()
-        item_dict["price"] = self.__price
+        item_dict["price"] = self.price
         item_dict["start_time"] = self.__start_time
         item_dict["end_time"] = self.__end_time
         return item_dict
-
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, new_price: float):
-        self.__price = new_price
 
     @property
     def time_range(self):
@@ -126,5 +122,5 @@ class Service(Product):
 
     def print_stats(self):
         super().print_stats()
-        print("Price: " + str(self.__price))
+        print("Price: " + str(self.price))
         print("Starting Availability: " + self.__start_time + "\nEnding Availability: " + self.__end_time)
